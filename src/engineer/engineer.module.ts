74ca585e57ecf376bd90ceabf220a4678ddf4c62
abdul.parawala <abdul@tallhaga.nu>
2026-06-12 18:05:37 +0530

    chore: remove misplaced backend files from root

--- src/engineer/engineer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EngineerResolver } from './engineer.resolver';
-import { EngineerService } from './engineer.service';
-
-@Module({
-  providers: [EngineerService, EngineerResolver],
-  exports: [EngineerService],
-})
-export class EngineerModule {}
--- src/queue/queue.constants.ts
+++
@@ -1,2 +0,0 @@
-export const PR_QUEUE = 'pr-processing';
-export const PROCESS_PR_JOB = 'process-pr';
